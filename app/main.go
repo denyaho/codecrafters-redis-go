f42868acfdf69c5f3eceb87a945288723192f08e
小川大地 <ogawadaichi@ogawadaichinoMacBook-Air.local>
2026-03-11 07:41:28 +0900

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"bufio"
+	"strings"
 )
 
 // Ensures gofmt doesn't remove the "net" and "os" imports in stage 1 (feel free to remove this!)
@@ -21,9 +23,28 @@ func main() {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
+	conn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
+	}
+	defer conn.Close()
+	for {
+		rawmessage, err := bufio.NewReader(conn).ReadString('\n')
+		if err != nil {
+			fmt.Println("Error reading from connection: ", err.Error())
+			break
+		}
+		messages := strings.TrimSpace(rawmessage)
+		if messages == "PING"{
+			conn.Write([]byte("+PONG\r\n"))
+		}else if strings.HasPrefix(messages, "SET"){
+			parts := strings.Fields(messages)
+			if len(parts) != 3 {
+				conn.Write([]byte("-ERR wrong number of arguments for 'SET' command\r\n"))
+			}else{
+				conn.Write([]byte("+OK\r\n"))
+			}
+		}
 	}
 }
