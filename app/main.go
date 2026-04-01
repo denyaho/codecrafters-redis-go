eba9b95cdacbcf34223b68b6986b1939edeee34f
小川大地 <ogawadaichi@ogawadaichinoMacBook-Air.local>
2026-04-02 01:52:22 +0900

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -8,11 +8,64 @@ import (
 	"strings"
 	"strconv"
 	"io"
+	"time"
+	"sync"
 )
 
 // Ensures gofmt doesn't remove the "net" and "os" imports in stage 1 (feel free to remove this!)
 var _ = net.Listen
 var _ = os.Exit
+
+type Item struct {
+	value string
+	expireAt int64
+}
+
+type ExpireMap struct {
+	data map[string]Item
+	mu sync.RWMutex
+}
+
+func (m *ExpireMap) Set(key, value string, expireAt time.Duration) {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+
+	var exp int64
+	if expireAt == 0 {
+		exp = 0
+	}else {
+		exp = time.Now().Add(expireAt).Unix()
+	}
+
+	m.data[key] = Item{
+		value: value,
+		expireAt: exp,
+	}
+}
+
+func (m *ExpireMap) Get(key string) (string, bool) {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+
+	item, ok := m.data[key]
+	if !ok {
+		return "", false
+	}
+	if item.expireAt != 0 && item.expireAt < time.Now().Unix() {
+		return "", false
+	}
+	return item.value, true
+}
+
+func (m *ExpireMap) Delete(key string) {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+
+	if _, ok :=m.data[key]; ok {
+		delete(m.data, key)
+	}
+}
+
 
 func reply_pong(conn net.Conn) {
 	defer conn.Close()
@@ -30,7 +83,11 @@ func reply_pong(conn net.Conn) {
 func handleConnection(conn net.Conn) {
 	defer conn.Close()
 	reader := bufio.NewReader(conn)
-	ans := make(map[string]string)
+	emap := &ExpireMap{
+		data: make(map[string]Item),
+		mu: sync.RWMutex{},
+	}
+
 	for {
 		message, err := reader.ReadString('\n')
 		if err != nil {
@@ -58,11 +115,24 @@ func handleConnection(conn net.Conn) {
 			if strings.ToUpper(args[0]) == "ECHO" {
 				conn.Write([]byte(fmt.Sprintf("$%d\r\n%s\r\n", len(args[1]), args[1])))
 			}else if strings.ToUpper(args[0]) == "SET" {
-				ans[args[1]] = args[2]
+				if len(args) > 4 && strings.ToUpper(args[3]) == "PX" {
+					expireMs, _ := strconv.Atoi(args[4])
+					expireAt := time.Duration(expireMs) * time.Millisecond
+					emap.Set(args[1], args[2], expireAt)
+				}else if len(args) > 4 && strings.ToUpper(args[3]) == "EX" {
+					expireS, _ := strconv.Atoi(args[4])
+					expireAt := time.Duration(expireS) * time.Second
+					emap.Set(args[1], args[2], expireAt)
+				}else{
+					emap.Set(args[1], args[2], 0)
+				}
 				conn.Write([]byte("+OK\r\n"))
 			}else if strings.ToUpper(args[0]) == "GET"{
-				if val, ok := ans[args[1]]; ok {
+				if val, ok := emap.Get(args[1]); ok {
 					conn.Write([]byte(fmt.Sprintf("$%d\r\n%s\r\n", len(val), val)))
+				} else {
+					emap.Delete(args[1])
+					conn.Write([]byte("$-1\r\n"))
 				}
 			}else {
 				conn.Write([]byte("+PONG\r\n"))
