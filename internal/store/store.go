3ceffd9a86f1640cbd00cf6d5092438ee94b9dc6
小川大地 <ogawadaichi@ogawadaichinoMacBook-Air.local>
2026-04-29 14:46:12 +0900

    codecrafters submit [skip ci]

--- internal/store/store.go
+++ internal/store/store.go
@@ -203,6 +203,7 @@ func (m *ExpireMap) BLPop(key string, timeout time.Duration) (string, bool, bool
 	m.signals[key] = ch
 	m.mu.Unlock()
 
+	fmt.Printf("timeout duration: %v\n", timeout)
 	var timer <- chan time.Time
 	if timeout > 0 {
 		timer = time.After(timeout)
