db7935610e54b6440ea5bfdcbf5fdc51c31ea8b4
小川大地 <ogawadaichi@ogawadaichinoMacBook-Air.local>
2026-04-29 12:47:59 +0900

    codecrafters submit [skip ci]

--- internal/resp/parser.go
+++ internal/resp/parser.go
@@ -43,7 +43,7 @@ func Parse(reader *bufio.Reader) ([]string, error)  {
 	if len(message) == 0 {
 		return nil, err
 	}
-	fmt.Printf("Received message: %s", message)
+	fmt.Printf("Received message: %c, %c\n", message[0], message[1])
 	switch message[0] {
 		case '+':
 			return []string{strings.TrimSpace(message[1:])}, nil
