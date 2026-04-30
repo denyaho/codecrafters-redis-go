912d58b4a9738892bea6bd1927d7d6caa4eca2ef
小川大地 <ogawadaichi@ogawadaichinoMacBook-Air.local>
2026-04-30 20:45:42 +0900

    codecrafters submit [skip ci]

--- internal/command/handleStream.go
+++ internal/command/handleStream.go
@@ -14,7 +14,10 @@ func handleType(st *store.ExpireMap, args []string) []byte {
 	if len(args) < 2 {
 		return []byte("-ERR wrong number of arguments for 'TYPE' command\r\n")
 	}
-	value, _ := st.Get(args[1])
+	value, ok := st.Get(args[1])
+	if !ok {
+		return []byte("+none\r\n")
+	}
 
 	switch value.(type) {	
 	case string:
