3254756e6f555fd544eaeb31e9246d0533c80abc
小川大地 <ogawadaichi@ogawadaichinoMacBook-Air.local>
2026-05-10 01:40:02 +0900

    codecrafters submit [skip ci]

--- internal/command/handleTransaction.go
+++ internal/command/handleTransaction.go
@@ -14,7 +14,7 @@ func handleINCR(st *store.ExpireMap, args []string) []byte {
 	val, ok := st.Get(key)
 	if !ok {
 		st.Set(key, "1", 0)
-		return []byte("+1\r\n")
+		return []byte(":1\r\n")
 	}
 	intValue,err := strconv.Atoi(val.(string))
 	if err != nil {
@@ -22,5 +22,5 @@ func handleINCR(st *store.ExpireMap, args []string) []byte {
 	}
 	intValue++
 	st.Set(key, strconv.Itoa(intValue), 0)
-	return []byte("+" + strconv.Itoa(intValue) + "\r\n")
+	return []byte(":" + strconv.Itoa(intValue) + "\r\n")
 }
