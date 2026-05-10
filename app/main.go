a9519456ab683ecd808c8dce581ccaa535cced74
小川大地 <ogawadaichi@ogawadaichinoMacBook-Air.local>
2026-05-10 14:08:31 +0900

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -14,8 +14,14 @@ var _ = os.Exit
 
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
+	port := "6379"
+	for i, arg := range os.Args {
+		if arg == "--port" && i+1 < len(os.Args) {
+			port = os.Args[i+1]
+		}
+	}
 	fmt.Println("Logs from your program will appear here!")
-	server := server.New("6379", "0.0.0.0")
+	server := server.New(port, "0.0.0.0")
 	server.StartServer()
 	// Uncomment the code below to pass the first stage
 	//
