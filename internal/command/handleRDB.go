7c533b419c218ecc711c1123a69ffb7f5b4dd1f8
小川大地 <ogawadaichi@ogawadaichinoMacBook-Air.local>
2026-05-24 21:53:59 +0900

    codecrafters submit [skip ci]

--- internal/command/handleRDB.go
+++ internal/command/handleRDB.go
@@ -9,9 +9,9 @@ func handleCONFIG(args []string, rdbConfig *rdb.RDB) []byte {
 	if args[1] == "GET" {
 		switch args[2] {
 		case "dir":
-			return resp.BuildBulkStrings(rdbConfig.Dir)
+			return resp.BuildArray([]string{"dir", rdbConfig.Dir})
 		case "dbfilename":
-			return resp.BuildBulkStrings(rdbConfig.DBfilename)
+			return resp.BuildArray([]string{"dbfilename", rdbConfig.DBfilename})
 		}
 	}
 	return resp.BuildError("ERR Unsupported CONFIG subcommand")
