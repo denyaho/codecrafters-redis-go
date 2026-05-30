216d91646ed9e237932b751848a165694a284abc
小川大地 <ogawadaichi@ogawadaichinoMacBook-Air.local>
2026-05-30 17:10:48 +0900

    codecrafters submit [skip ci]

--- internal/command/handlePubSub.go
+++ internal/command/handlePubSub.go
@@ -2,10 +2,10 @@ package handler
 
 import (
 	"github.com/codecrafters-io/redis-starter-go/internal/resp"
+	"fmt"
 )
 
 func handleSUBSCRIBE(args []string) []byte {
-	response := resp.BuildArray([]string{"subscribe", args[1]})
-	response = append(response, resp.BuildInteger(1)...)
+	response := resp.BuildArray([]string{"subscribe", args[1], fmt.Sprintf(":%d", 1)})
 	return response
 }
