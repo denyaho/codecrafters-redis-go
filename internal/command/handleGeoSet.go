8399a2bd8b8347976761c154c7a68b1988bab5a3
小川大地 <ogawadaichi@ogawadaichinoMacBook-Air.local>
2026-06-04 23:53:05 +0900

    codecrafters submit [skip ci]

--- internal/command/handleGeoSet.go
+++ internal/command/handleGeoSet.go
@@ -4,7 +4,13 @@ import (
 	"github.com/codecrafters-io/redis-starter-go/internal/resp"
 	"github.com/codecrafters-io/redis-starter-go/internal/store"
 	"strconv"
+	"fmt"
 )
+
+var MaxLongitude = 180.0
+var MinLongitude = -180.0
+var MaxLatitude = 85.05112878
+var MinLatitude = -85.05112878
 
 func handleGEOADD(st *store.ExpireMap, args []string) []byte {
 	if len(args) != 5 {
@@ -19,6 +25,13 @@ func handleGEOADD(st *store.ExpireMap, args []string) []byte {
 	latitude, err := strconv.ParseFloat(args[3], 64)
 	if err != nil {
 		return resp.BuildError("ERR value is not a valid float")
+	}
+
+	if longitude < MinLongitude || longitude > MaxLongitude {
+		return resp.BuildError(fmt.Sprintf("ERR invalid longitude latitude pair %.6f, %.6f", longitude, latitude))
+	}
+	if latitude < MinLatitude || latitude > MaxLatitude {
+		return resp.BuildError(fmt.Sprintf("ERR invalid longitude latitude pair %.6f, %.6f", longitude, latitude))
 	}
 	member := args[4]
 	val, err := st.GeoAdd(key, longitude, latitude, member)
