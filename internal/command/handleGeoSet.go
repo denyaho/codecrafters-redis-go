10bfeddf40dc492565c5ec056a1f757568503d24
小川大地 <ogawadaichi@ogawadaichinoMacBook-Air.local>
2026-06-04 23:46:12 +0900

    codecrafters submit [skip ci]

---
+++ internal/command/handleGeoSet.go
@@ -0,0 +1,29 @@
+package handler
+
+import (
+	"github.com/codecrafters-io/redis-starter-go/internal/resp"
+	"github.com/codecrafters-io/redis-starter-go/internal/store"
+	"strconv"
+)
+
+func handleGEOADD(st *store.ExpireMap, args []string) []byte {
+	if len(args) != 5 {
+		return resp.BuildError("ERR wrong number of arguments for 'GEOADD' command")
+	}
+
+	key := args[1]
+	longitude, err := strconv.ParseFloat(args[2], 64)
+	if err != nil {
+		return resp.BuildError("ERR value is not a valid float")
+	}
+	latitude, err := strconv.ParseFloat(args[3], 64)
+	if err != nil {
+		return resp.BuildError("ERR value is not a valid float")
+	}
+	member := args[4]
+	val, err := st.GeoAdd(key, longitude, latitude, member)
+	if err != nil {
+		return resp.BuildError("ERR could not add geo data")
+	}
+	return resp.BuildInteger(val)
+}
