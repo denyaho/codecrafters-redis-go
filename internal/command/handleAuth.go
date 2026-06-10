10c4100de22ea1c9b496bc8b0eda9945304e18f9
小川大地 <ogawadaichi@ogawadaichinoMacBook-Air.local>
2026-06-11 04:44:57 +0900

    codecrafters submit [skip ci]

--- internal/command/handleAuth.go
+++ internal/command/handleAuth.go
@@ -22,7 +22,9 @@ func handleACLWhoami(st *store.ExpireMap, args []string, c *pubsub.Client) []byt
 }
 
 func handleACLGetUser(st *store.ExpireMap, args []string, c *pubsub.Client) []byte {
-	return resp.BuildArray([]string{"flags", ""})
+	response := resp.BuildBulkStrings("flags")
+	response = append(response, resp.BuildArray([]string{})...)
+	return response
 }
 
 
